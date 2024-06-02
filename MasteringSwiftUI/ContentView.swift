cff6838ce6ae76be91ef523bc461c421015cf66b
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2024-06-02 17:53:10 +0900

    complete

--- MasteringSwiftUI/ContentView.swift
+++ MasteringSwiftUI/ContentView.swift
@@ -6,21 +6,42 @@
 //
 
 import SwiftUI
+import DebugFrame
+
+//final class ContentViewModel: ObservableObject {
+//
+//    @Published var counter: Int = 99
+//}
+
+
+@Observable
+final class ContentViewModel {
+
+    var counter: Int = 99 {
+        didSet {
+            print(counter)
+        }
+    }
+
+    var values: [Int] = [Int](1...100_000)
+}
 
 struct ContentView: View {
 
-    @State private var counter: Int = 99
+    var model: ContentViewModel
 
-    init() {
+    init(_ model: ContentViewModel) {
+        self.model = model
         print(Self.self, #function)
     }
 
     var body: some View {
         let _ = Self._printChanges()
         VStack {
-//            Text("Counter: \(counter)")
+            ContentListView(values: model.values)
+            Text("Counter: \(model.counter)")
             Button {
-                counter += 1
+                model.counter += 1
             } label: {
                 Text("Counter +1")
             }
@@ -31,6 +52,37 @@ struct ContentView: View {
     }
 }
 
+// MARK: - Inside types
+
+private extension ContentView {
+
+    struct ContentListView: View, Equatable {
+
+        let values: [Int]
+
+        init(values: [Int]) {
+            self.values = values
+            print(Self.self, #function)
+        }
+
+        var body: some View {
+            let _ = Self._printChanges()
+            List {
+                ForEach(values, id: \.self) {
+                    Text(String($0))
+                        .padding()
+                        .debugBackground()
+                }
+            }
+            .listStyle(.plain)
+            .frame(height: 200.0)
+            .padding()
+            .debugBackground()
+        }
+    }
+}
+
+@available(iOS 17, *)
 #Preview {
-    ContentView()
+    ContentView(ContentViewModel())
 }
--- MasteringSwiftUI/MasteringSwiftUIApp.swift
+++ MasteringSwiftUI/MasteringSwiftUIApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct MasteringSwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(ContentViewModel())
         }
     }
 }
